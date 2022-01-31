6ba6770b389961dc7a6a793c66bac381e03868b7
Houssem Lahiani <lahianihoussem@gmail.com>
2022-01-31 22:21:59 +0100

    final commit

--- src/app/hauptmaschinedetails/hauptmaschine1/jour/jour.page.ts
+++ src/app/hauptmaschinedetails/hauptmaschine1/jour/jour.page.ts
@@ -33,6 +33,7 @@ export class JourPage implements OnInit {
   backpage(){
     this.route.navigate(['/hauptmaschinedetails/hauptmaschine1'])
   }
+  
   pdfDownload() {
     
     const docDef = {
@@ -70,10 +71,10 @@ export class JourPage implements OnInit {
         {text: 'Datum :',margin: [0, 45, 0, 0], fontSize:18},
                 {text: this.Datum,margin: [150, -20, 0, 0], fontSize:16,bold:true},
                 {text: 'Teppich :',margin: [0, 45, 0, 0], fontSize:18},
-        {text: 'Gewechselt',margin: [150, -20, 0, 0], fontSize:16,bold:true},
+        {text: this.Teppich,margin: [150, -20, 0, 0], fontSize:16,bold:true},
        
 {text: 'Schrauben am Rollenregal :',margin: [0, 45, 0, 0], fontSize:18},
-        {text: 'Kontrolliert',margin: [250, -20, 0, 0], fontSize:16,bold:true},
+        {text: this.Kuhlmittel,margin: [250, -20, 0, 0], fontSize:16,bold:true},
                
            
                
@@ -209,7 +210,7 @@ export class JourPage implements OnInit {
       
         this.file.resolveDirectoryUrl(this.file.externalCacheDirectory)
         .then(dirEntry => {
-          this.file.getFile(dirEntry, Date.now().toLocaleString+'.pdf', { create: true })
+          this.file.getFile(dirEntry, Date.now()+'.pdf', { create: true })
             .then(fileEntry => {
               fileEntry.createWriter(writer => {
                 writer.onwrite = () => {
@@ -253,85 +254,5 @@ export class JourPage implements OnInit {
       
     }  
   
-    /*  pdfMake.createPdf(docDef).getBuffer((buffer) => {
-        var utf8 = new Uint8Array(buffer); // Convert to UTF-8...
-        let binaryArray = utf8.buffer; // Convert to Binary...
-
-        let fileName = "file.pdf";
-        let saveDir = this.file.externalCacheDirectory;
-
-          this.file.createFile(saveDir, fileName, true).then((fileEntry) => {
-            fileEntry.createWriter((fileWriter) => {
-                fileWriter.onwriteend = () => {
-                 
-                  
-                    this.fileOpener.open(
-                      saveDir+fileName,
-                      'application/pdf');
-                 
-                };
-                fileWriter.onerror = (e) => {
-                    console.log('file writer - error event fired: ' + e.toString());
-                };
-                fileWriter.write(binaryArray);
-            });
-          });
-        });
-     
-  }
-/*
-  createPdf() {
-    const pdfBlock = document.getElementById("print-wrapper");
-    
-    const options = { 
-      background: "white", 
-      height: pdfBlock.clientWidth, 
-      width: pdfBlock.clientHeight 
-    };
-
-    domtoimage.toPng(pdfBlock, options).then((fileUrl) => {
-      var doc = new JSPDF("p","mm","a4");
-      doc.addImage(fileUrl, 'PNG', 10, 10, 240, 180);
-  
-      let docRes = doc.output();
-      let buffer = new ArrayBuffer(docRes.length);
-      let array = new Uint8Array(buffer);
-      for (var i = 0; i < docRes.length; i++) {
-          array[i] = docRes.charCodeAt(i);
-      }
-  
-  
-      const directory = this.file.dataDirectory;
-      const fileName = "user-data.pdf";
-
-      let options: IWriteOptions = { 
-        replace: true 
-      };
-  
-      this.file.checkFile(directory, fileName).then((res)=> {
-        this.file.writeFile(directory, fileName,buffer, options)
-        .then((res)=> {
-          console.log("File generated" + JSON.stringify(res));
-          this.fileOpener.open(this.file.dataDirectory + fileName, 'application/pdf')
-            .then(() => console.log('File is exported'))
-            .catch(e => console.log(e));
-        }).catch((error)=> {
-          console.log(JSON.stringify(error));
-        });
-      }).catch((error)=> {
-        this.file.writeFile(directory,fileName,buffer).then((res)=> {
-          console.log("File generated" + JSON.stringify(res));
-          this.fileOpener.open(this.file.dataDirectory + fileName, 'application/pdf')
-            .then(() => console.log('File exported'))
-            .catch(e => console.log(e));
-        })
-        .catch((error)=> {
-          console.log(JSON.stringify(error));
-        });
-      });
-    }).catch(function (error) {
-      console.error(error);
-    });
-  } 
-  */
+   
 }
